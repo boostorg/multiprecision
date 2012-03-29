ec3cf57c914487c57da2c8669e5e200480732d19
John Maddock <john@johnmaddock.co.uk>
2012-03-29 12:15:05 +0000

    Add licence and copyright.
    [SVN r77624]

--- test/test.hpp
+++ test/test.hpp
@@ -1,3 +1,8 @@
+///////////////////////////////////////////////////////////////
+//  Copyright 2012 John Maddock. Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_
+//
 
 #ifndef BOOST_MULTIPRECISION_TEST_HPP
 #define BOOST_MULTIPRECISION_TEST_HPP
