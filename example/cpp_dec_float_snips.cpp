43975488162e679e870e30e66f6224b7d6b8dec1
John Maddock <john@johnmaddock.co.uk>
2012-03-29 12:28:31 +0000

    Extend examples using Paul Bristow's comments.
    [SVN r77626]

--- example/cpp_dec_float_snips.cpp
+++ example/cpp_dec_float_snips.cpp
@@ -17,12 +17,18 @@ void t1()
    // Operations at fixed precision and full numeric_limits support:
    cpp_dec_float_100 b = 2;
    std::cout << std::numeric_limits<cpp_dec_float_100>::digits << std::endl;
-   // We can use any C++ std lib function:
-   std::cout << log(b) << std::endl; // print log(2)
+   // Note that digits10 is the same as digits, since we're base 10! :
+   std::cout << std::numeric_limits<cpp_dec_float_100>::digits10 << std::endl;
+   // We can use any C++ std lib function, lets print all the digits as well:
+   std::cout << std::setprecision(std::numeric_limits<cpp_dec_float_100>::max_digits10) 
+      << log(b) << std::endl; // print log(2)
    // We can also use any function from Boost.Math:
    std::cout << boost::math::tgamma(b) << std::endl;
    // These even work when the argument is an expression template:
    std::cout << boost::math::tgamma(b * b) << std::endl;
+   // And since we have an extended exponent range we can generate some really large 
+   // numbers here (4.0238726007709377354370243e+2564):
+   std::cout << boost::math::tgamma(cpp_dec_float_100(1000)) << std::endl;
    //]
 }
 
