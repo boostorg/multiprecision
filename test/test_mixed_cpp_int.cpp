77d07eb7e988c80aa3520c222f75b54fc69a8543
John Maddock <john@johnmaddock.co.uk>
2012-11-20 12:00:15 +0000

    Fix Intel compile failure
    [SVN r81434]

--- test/test_mixed_cpp_int.cpp
+++ test/test_mixed_cpp_int.cpp
@@ -43,9 +43,11 @@ void test()
    //
    // Try again with integer types as the source:
    //
-   static const unsigned max_digits = std::numeric_limits<test_type>::is_signed ? std::numeric_limits<long long>::digits : std::numeric_limits<unsigned long long>::digits;
-   static const unsigned require_digits = std::numeric_limits<test_type>::digits <= 2 * max_digits ? std::numeric_limits<test_type>::digits / 2 : max_digits;
-   typedef typename boost::mpl::if_c<std::numeric_limits<test_type>::is_signed, typename boost::int_t<require_digits>::least, typename boost::uint_t<require_digits>::least>::type i_type;
+   enum{ max_digits = std::numeric_limits<test_type>::is_signed ? std::numeric_limits<long long>::digits : std::numeric_limits<unsigned long long>::digits };
+   enum{ require_digits = std::numeric_limits<test_type>::digits <= 2 * max_digits ? std::numeric_limits<test_type>::digits / 2 : max_digits };
+   typedef typename boost::uint_t<require_digits>::least uint_least;
+   typedef typename boost::int_t<require_digits>::least  int_least;
+   typedef typename boost::mpl::if_c<std::numeric_limits<test_type>::is_signed, int_least, uint_least>::type i_type;
 
    i_type ih = (std::numeric_limits<i_type>::max)();
    i_type il = (std::numeric_limits<i_type>::max)();
@@ -80,7 +82,7 @@ int main()
    test<checked_uint256_t, checked_uint512_t>();
    test<number<cpp_int_backend<64, 64, unsigned_magnitude, checked, void>, et_off>, checked_uint128_t>();
    test<boost::uint64_t, checked_int128_t>();
-   
+
    return boost::report_errors();
 }
 
