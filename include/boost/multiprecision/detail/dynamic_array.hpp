726b2ffc32cb48d88a5bc8ac0baf1e425dcdbc52
Christopher Kormanyos <e_float@yahoo.com>
2013-03-10 11:04:09 +0000

    Proper copyright information + foundations for cpp_bin_float.
    [SVN r83395]

--- include/boost/multiprecision/detail/dynamic_array.hpp
+++ include/boost/multiprecision/detail/dynamic_array.hpp
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
-//  Copyright Christopher Kormanyos 2013.
-//  Copyright 2012 John Maddock. Distributed under the Boost
+//  Copyright 2012 John Maddock.
+//  Copyright Christopher Kormanyos 2013. Distributed under the Boost
 //  Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
