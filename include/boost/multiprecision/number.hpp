17f5ed45c24001b2283dedde8d003a8aaef80ec6
Marcel Raad <MarcelRaad@users.noreply.github.com>
2015-04-02 11:38:48 +0200

    Suppress MSVC warning
    warning C4244: 'argument': conversion from 'int' to 'char', possible loss of data
    
    This caused the Boost.Units regression tests to fail on MSVC because of warnings-as-errors.

--- include/boost/multiprecision/number.hpp
+++ include/boost/multiprecision/number.hpp
@@ -1735,7 +1735,7 @@ inline std::string read_string_while(std::istream& is, std::string const& permit
          else if(permitted_chars.find_first_of(std::istream::traits_type::to_char_type(c)) == std::string::npos)
          {
             // Invalid numeric character, stop reading:
-            is.rdbuf()->sputbackc(c);
+            is.rdbuf()->sputbackc(static_cast<char>(c));
             break;
          }
          else
