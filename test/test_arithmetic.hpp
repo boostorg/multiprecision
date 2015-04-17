689e66d28d4264e8e37b2c889f6eb10976c0bef9
jzmaddock <john@johnmaddock.co.uk>
2015-04-17 08:30:45 +0100

    Fix self assignment bug in mpfr_float Fixes: https://svn.boost.org/trac/boost/ticket/11193

--- test/test_arithmetic.hpp
+++ test/test_arithmetic.hpp
@@ -1905,5 +1905,11 @@ void test()
    // Destructor of "a" checks destruction of moved-from-object...
    Real m3(static_cast<Real&&>(a));
 #endif
+   //
+   // Bug cases, self assignment first:
+   //
+   a = 20;
+   a = a;
+   BOOST_CHECK_EQUAL(a, 20);
 }
 
