9e4b524d00437c64dd9049c6f90721889f5f48f3
jzmaddock <john@johnmaddock.co.uk>
2017-12-26 17:03:03 +0000

    Add test cases for fixed bugs.

---
+++ test/git_issue_30.cpp
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright 2016 John Maddock. Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <boost/multiprecision/cpp_int.hpp>
+
+struct E
+{
+   E(boost::multiprecision::cpp_rational const&)
+   {
+
+   }
+};
+
+void g(boost::multiprecision::cpp_rational const& r)
+{
+   std::cout << r << std::endl;
+}
+
+int main()
+{
+   boost::multiprecision::cpp_int r = 3;
+   g(r);
+   E x1(r);  // triggers explicit conversion operator.
+   return 0;
+}
+
