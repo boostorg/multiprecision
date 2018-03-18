458e2b90da06b5357cdfdd32efd4a16dd34f019d
Nick Thompson <nathompson7@protonmail.com>
2018-03-18 20:44:38 +0800

    [CI SKIP] One more failing test.

--- test/test_mpc_overloads.cpp
+++ test/test_mpc_overloads.cpp
@@ -1,3 +1,4 @@
+#include <type_traits>
 #include "test.hpp"
 #include <boost/multiprecision/mpc.hpp>
 
@@ -6,9 +7,10 @@ using boost::multiprecision::mpc_complex_100;
 template<class Complex>
 void test_overloads()
 {
+  typedef typename Complex::value_type Real;
   Complex ya = {5.2, 7.4};
   Complex yb = {8.2, 7.3};
-  typename Complex::value_type h = 0.0001;
+  Real h = 0.0001;
   auto I0 = (ya + yb)*h;
   Complex I1 = I0/2 + yb*h;
 
@@ -19,9 +21,19 @@ void test_overloads()
   int n = 2;
   using std::sin;
   Complex arg = z*sin(theta) - n*theta;
+
+
+  using std::exp;
+  Real v = 0.2;
+  Real cotv = 7.8;
+  Real cscv = 8.2;
+  Complex den = z + v*cscv*exp(-v*cotv);
 }
+
+
 
 int main()
 {
   test_overloads<mpc_complex_100>();
+  
 }
