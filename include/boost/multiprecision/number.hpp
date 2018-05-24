0698fec7d63e76ba63991722510dd999a7074cb8
jzmaddock <john@johnmaddock.co.uk>
2018-05-24 12:52:45 +0100

    Revert "temporary commit to test workaround: REVERT ME!!!"
    This reverts commit 683ea7fa34d4093600a7a345e92a01895c9955fd.
    
    [CI SKIP]

--- include/boost/multiprecision/number.hpp
+++ include/boost/multiprecision/number.hpp
@@ -639,10 +639,10 @@ public:
       return this->template convert_to<T>();
    }
 #  else
-#if 1//BOOST_WORKAROUND(BOOST_MSVC, < 1900)
+#if BOOST_WORKAROUND(BOOST_MSVC, < 1900)
    template <class T>
 #else
-   template <class T, class = typename boost::disable_if_c<boost::is_constructible<T, self_type const&>::value || !boost::is_default_constructible<T>::value, T>::type>
+   template <class T, class = typename boost::disable_if_c<std::is_constructible<T, self_type const&>::value || !std::is_default_constructible<T>::value, T>::type>
 #endif
    explicit operator T ()const
    {
