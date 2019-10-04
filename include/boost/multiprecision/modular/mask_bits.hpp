7dc3f5ac3f37a881633dbdd7162b809fd1657675
Zerg1996 <zerg1996@yandex.ru>
2019-10-04 16:50:15 +0300

    Mask_bits function for all backend #2

--- include/boost/multiprecision/modular/mask_bits.hpp
+++ include/boost/multiprecision/modular/mask_bits.hpp
@@ -11,7 +11,6 @@
 #ifndef BOOST_MULTIPRECISION_MASK_BITS_HPP
 #define BOOST_MULTIPRECISION_MASK_BITS_HPP
 
-#include <boost/multiprecision/cpp_int.hpp>
 #include <boost/multiprecision/detail/number_base.hpp>
 #include <boost/multiprecision/number.hpp>
 
@@ -21,27 +20,32 @@ namespace multiprecision {
 template <typename Backend, typename Integer>
 void eval_mask_bits(Backend& val, Integer n)
 {
-
    typedef typename boost::multiprecision::detail::canonical<unsigned, Backend>::type ui_type;
-   static const ui_type                                                               zero = 0u;
-
-   if (n == 0)
-   {
-      val = zero;
-      return;
-   }
-
-   const size_t    top_word = n / Backend::limb_bits;
-   const limb_type mask     = (limb_type(1) << (n % Backend::limb_bits)) - 1;
-
-   if (top_word < val.size())
-   {
-      for (size_t i = top_word + 1; i < val.size(); ++i)
-      {
-         val.limbs()[i] = 0;
-      }
-      val.limbs()[top_word] &= mask;
-   }
+   static const ui_type one = 1u;
+   Backend tmp;
+   backends::eval_bit_set(tmp, n);
+   backends::eval_subtract(tmp, one);
+   backends::eval_bitwise_and(val, tmp);
+//   typedef typename boost::multiprecision::detail::canonical<unsigned, Backend>::type ui_type;
+//   static const ui_type                                                               zero = 0u;
+//
+//   if (n == 0)
+//   {
+//      val = zero;
+//      return;
+//   }
+//
+//   const size_t    top_word = n / Backend::limb_bits;
+//   const limb_type mask     = (limb_type(1) << (n % Backend::limb_bits)) - 1;
+//
+//   if (top_word < val.size())
+//   {
+//      for (size_t i = top_word + 1; i < val.size(); ++i)
+//      {
+//         val.limbs()[i] = 0;
+//      }
+//      val.limbs()[top_word] &= mask;
+//   }
 }
 }
 } // namespace boost::multiprecision
--- test/test_modular_adaptor.cpp
+++ test/test_modular_adaptor.cpp
@@ -13,6 +13,8 @@
 #include <boost/multiprecision/cpp_modular.hpp>
 #include <boost/multiprecision/cpp_complex.hpp>
 #include <chrono>
+#include <boost/multiprecision/tommath/modular_params_tommath.hpp>
+
 using namespace boost::multiprecision;
 
 int main() {
@@ -24,10 +26,18 @@ int main() {
    cpp_mod base_m(base, mod);
    cpp_mod exp_m(exp, exp_1);
 
+//   tom_int baset = 2234234234234;
+//   tom_int expt("34534534534532342342342344534524");
+//   tom_int exp_1t = expt + 1;
+//   tom_int modt = 25234234234234;
+//   tom_int_mod base_mt(baset, modt);
+//   tom_int_mod exp_mt(expt, exp_1t);
+
    auto t1 = std::chrono::high_resolution_clock::now();
    std::cout << powm(base, exp, mod) << std::endl;
    auto t2 = std::chrono::high_resolution_clock::now();
-   std::cout << pow(base_mt, exp_mt) << std::endl;
+   std::cout << pow(base_m, exp_m) << std::endl;
+//   std::cout << pow(base_mt, exp_mt) << std::endl;
    auto t3 = std::chrono::high_resolution_clock::now();
    auto duration_zerg = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
    auto duration_boost = std::chrono::duration_cast<std::chrono::microseconds>( t3 - t2 ).count();
