e51c4877120d26a687a3a56b6811f171c0356a7c
pabristow <pbristow@hetp.u-net.com>
2020-01-30 16:24:09 +0000

    Rebuild with mods to big_seventh.

--- example/big_seventh.cpp
+++ example/big_seventh.cpp
@@ -54,7 +54,7 @@ int main()
 {
 
 //[big_seventh_example_2
-/*`Using `typedef cpp_bin_float_50` hides the complexity of multiprecision,
+/*`Using a `typedef` like `cpp_bin_float_50` hides the complexity of multiprecision, and
 allows us to define variables with 50 decimal digit precision just like __fundamental `double`.
 */
   using boost::multiprecision::cpp_bin_float_50;
@@ -62,9 +62,9 @@ allows us to define variables with 50 decimal digit precision just like __fundam
   cpp_bin_float_50 seventh = cpp_bin_float_50(1) / 7;  // 1 / 7
 
 /*`By default, output would only show the standard 6 decimal digits,
- so set precision to show all 50 significant digits, including any trailing zeros.
+ so set precision to show all 50 significant digits, including any trailing zeros (to show the full implied 50 digit precision).
 */
-  std::cout.precision(std::numeric_limits<cpp_bin_float_50>::digits10);
+  std::cout.precision(std::numeric_limits<cpp_bin_float_50>::digits10); // Show 50 decimal digit precision.
   std::cout << std::showpoint << std::endl; // Append any trailing zeros.
   std::cout << seventh << std::endl;
 /*`which outputs:
@@ -90,7 +90,7 @@ guaranteed to be initialized with the very last bit of precision for the floatin
 /*`So using `cpp_bin_float_50` looks like a simple 'drop-in' for the __fundamental_type like 'double',
 but beware of less-than-expected precision from construction or conversion from `double` or other lower precision types.
 This is a mistake that is very easy to make,
-and very difficult to detect because the difference in precision is only visible after the 17th decimal digit.
+and very difficult to detect because the difference in precision is only visible after about the 17th decimal digit.
 
 We can show this by constructing a fraction one seventh from `double`:
 */
@@ -105,23 +105,22 @@ We can show this by constructing a fraction one seventh from `double`:
 /*`Did you spot the probably-unintended difference?  After the 17th decimal digit, result is apparently random
 and not the expected recurring pattern 14285714285714...
 
-The 'random' digits after digit 17 are from the cpp_bin_float50 representation of the double value 0.14285714285714
-which is different from the 'better' cpp_bin_float50 representation of the fraction 1/7
+The 'random' digits after digit 17 are from the `cpp_bin_float_50` representation of the `double` value 0.14285714285714
+which is different from the 'better' `cpp_bin_float_50` representation of the fraction 1/7
 */
 
    cpp_bin_float_50 seventh_big(1);  // 1
    seventh_big /= 7;
    std::cout << "seventh_big = " << seventh_big << std::endl; //
    // seventh_big     = 0.14285714285714285714285714285714285714285714285714
-//` Note the recurring 14285714285714 pattern as expected.
+//`Note the recurring 14285714285714 pattern as expected.
 
-//` We can also construct a const version (but not yet constexpr) and evaluate in a single expression:
+//`We can also construct a `const` version (but not yet `constexpr`) and evaluate in a single expression:
 
    const cpp_bin_float_50 seventh_const (cpp_bin_float_50(1) / 7);
    std::cout << "seventh_const = " << seventh_const << std::endl; //
    // seventh_const = 0.14285714285714285714285714285714285714285714285714
-
-//]  [/big_seventh_example_3
+//]  [/big_seventh_example_3]
 
 //[big_seventh_example_constexpr
 
@@ -133,7 +132,30 @@ which is different from the 'better' cpp_bin_float50 representation of the fract
    // nor use the macro BOOST_CONSTEXPR_OR_CONST unless it returns `const`
    // BOOST_CONSTEXPR_OR_CONST cpp_bin_float_50 seventh_constexpr(seventh_const);
 
-//]  [/big_seventh_example_constexpr
+//]  [/big_seventh_example_constexpr]
+
+//[big_seventh_example_4
+   /*`For some purposes, this difference in precision may be insignificant,
+but if one is implementing a formula involving a fraction from integers,
+including decimal fractions like 1/10, 1/100, then comparison with other computations like __WolframAlpha 
+will reveal differences whose cause may be perplexing. 
+
+To get as precise-as-possible decimal fractions like 1.234, we can write
+*/
+
+  const cpp_bin_float_50 f1(cpp_bin_float_50(1234) / 100);  // Construct from a fraction.
+  std::cout << "cpp_bin_float_50 f1(cpp_bin_float_50(1234) / 100) = " << f1 << std::endl; // 12.340000000000000000000000000000000000000000000000
+/*`or
+*/
+  const cpp_bin_float_50 f2("1.234");  // Construct from decimal digit string.
+  std::cout << "cpp_bin_float_50 f2(\"1.234\") = " << f2 << std::endl; // 12.340000000000000000000000000000000000000000000000
+
+/*`that are different from constructing from a `double` with value 1.234
+*/
+  const cpp_bin_float_50 f3(cpp_bin_float_50(1.234));
+  std::cout << "cpp_bin_float_50 f3(cpp_bin_float_50(1.234)) = " << f3 << std::endl; // 1.2339999999999999857891452847979962825775146484375
+   
+//]  [/big_seventh_example_4]
 
     return 0;
 } // int main()
@@ -145,11 +167,13 @@ which is different from the 'better' cpp_bin_float50 representation of the fract
 0.14285714285714285714285714285714285714285714285714
 pi = 3.1415926535897932384626433832795028841971693993751
 c =  0.89759790102565521098932668093700082405633411410717
-d7 = 0.14285714285714284921269268124888185411691665649414
 seventh_0 = 0.0000000000000000000000000000000000000000000000000
 seventh_double = 0.14285714285714284921269268124888185411691665649414
 seventh_big = 0.14285714285714285714285714285714285714285714285714
 seventh_const = 0.14285714285714285714285714285714285714285714285714
+cpp_bin_float_50 f1(cpp_bin_float_50(1234) / 100) = 12.340000000000000000000000000000000000000000000000
+cpp_bin_float_50 f2("1.234") = 1.2340000000000000000000000000000000000000000000000
+cpp_bin_float_50 f3(cpp_bin_float_50(1.234)) = 1.2339999999999999857891452847979962825775146484375
 
 //] //[big_seventh_example_output]
 
