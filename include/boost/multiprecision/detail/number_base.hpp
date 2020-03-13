9710adb8f7080b7124fc8e13d127768e21eefd79
jzmaddock <john@johnmaddock.co.uk>
2020-03-13 18:20:22 +0000

    Add a way to disable C++03 deprecation warnings. [CI SKIP]

--- include/boost/multiprecision/detail/number_base.hpp
+++ include/boost/multiprecision/detail/number_base.hpp
@@ -33,9 +33,10 @@
 // As from March 2020, C++03 support is deprecated, and as from March 2021 will be removed,
 // so mark up as such:
 //
-#if defined(_MSC_VER) || defined(__GNUC__)
+#if (defined(_MSC_VER) || defined(__GNUC__)) && !defined(BOOST_MP_DISABLE_DEPRECATE_03_WARNING)
 #pragma message("CAUTION: One or more C++11 features were found to be unavailable")
 #pragma message("CAUTION: Compiling Boost.Multiprecision in non-C++11 or later conformance modes is now deprecated and will be removed from March 2021.")
+#pragma message("CAUTION: Define BOOST_MP_DISABLE_DEPRECATE_03_WARNING to suppress this message.")
 #endif
 #endif
 
