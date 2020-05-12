9ca27587adb2d827a757893de48f48fc0d5de1e8
jzmaddock <john@johnmaddock.co.uk>
2020-05-12 21:59:58 +0100

    Faster addition using Intel intrinsics. Replaces: https://github.com/boostorg/multiprecision/pull/233.

---
+++ include/boost/multiprecision/cpp_int/add_unsigned.hpp
@@ -0,0 +1,163 @@
+///////////////////////////////////////////////////////////////
+//  Copyright 2020 John Maddock. Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef BOOST_MP_ADD_UNSIGNED_ADDC_32_HPP
+#define BOOST_MP_ADD_UNSIGNED_ADDC_32_HPP
+
+#include <boost/multiprecision/cpp_int/intel_intrinsics.hpp>
+
+namespace boost { namespace multiprecision { namespace backends {
+
+template <class CppInt1, class CppInt2, class CppInt3>
+inline BOOST_MP_CXX14_CONSTEXPR void add_unsigned_constexpr(CppInt1& result, const CppInt2& a, const CppInt3& b) BOOST_MP_NOEXCEPT_IF(is_non_throwing_cpp_int<CppInt1>::value)
+{
+   using ::boost::multiprecision::std_constexpr::swap;
+
+   // Nothing fancy, just let uintmax_t take the strain:
+   double_limb_type carry = 0;
+   unsigned         m(0), x(0);
+   unsigned         as = a.size();
+   unsigned         bs = b.size();
+   minmax(as, bs, m, x);
+   if (x == 1)
+   {
+      bool s = a.sign();
+      result = static_cast<double_limb_type>(*a.limbs()) + static_cast<double_limb_type>(*b.limbs());
+      result.sign(s);
+      return;
+   }
+   result.resize(x, x);
+   typename CppInt2::const_limb_pointer pa     = a.limbs();
+   typename CppInt3::const_limb_pointer pb     = b.limbs();
+   typename CppInt1::limb_pointer       pr     = result.limbs();
+   typename CppInt1::limb_pointer       pr_end = pr + m;
+
+   if (as < bs)
+      swap(pa, pb);
+
+   // First where a and b overlap:
+   while (pr != pr_end)
+   {
+      carry += static_cast<double_limb_type>(*pa) + static_cast<double_limb_type>(*pb);
+#ifdef __MSVC_RUNTIME_CHECKS
+      *pr = static_cast<limb_type>(carry & ~static_cast<limb_type>(0));
+#else
+      *pr = static_cast<limb_type>(carry);
+#endif
+      carry >>= CppInt1::limb_bits;
+      ++pr, ++pa, ++pb;
+   }
+   pr_end += x - m;
+   // Now where only a has digits:
+   while (pr != pr_end)
+   {
+      if (!carry)
+      {
+         if (pa != pr)
+            std_constexpr::copy(pa, pa + (pr_end - pr), pr);
+         break;
+      }
+      carry += static_cast<double_limb_type>(*pa);
+#ifdef __MSVC_RUNTIME_CHECKS
+      *pr = static_cast<limb_type>(carry & ~static_cast<limb_type>(0));
+#else
+      *pr = static_cast<limb_type>(carry);
+#endif
+      carry >>= CppInt1::limb_bits;
+      ++pr, ++pa;
+   }
+   if (carry)
+   {
+      // We overflowed, need to add one more limb:
+      result.resize(x + 1, x + 1);
+      if (result.size() > x)
+         result.limbs()[x] = static_cast<limb_type>(carry);
+   }
+   result.normalize();
+   result.sign(a.sign());
+}
+
+
+#ifdef BOOST_MP_HAS_IMMINTRIN_H
+//
+// This is the key addition routine where all the argument types are non-trivial cpp_int's:
+//
+template <class CppInt1, class CppInt2, class CppInt3>
+inline BOOST_MP_CXX14_CONSTEXPR void add_unsigned(CppInt1& result, const CppInt2& a, const CppInt3& b) BOOST_MP_NOEXCEPT_IF(is_non_throwing_cpp_int<CppInt1>::value)
+{
+#ifndef BOOST_MP_NO_CONSTEXPR_DETECTION
+   if (BOOST_MP_IS_CONST_EVALUATED(a.size()))
+   {
+      add_unsigned_constexpr(result, a, b);
+   }
+   else
+#endif
+   {
+      using ::boost::multiprecision::std_constexpr::swap;
+
+      // Nothing fancy, just let uintmax_t take the strain:
+      unsigned m(0), x(0);
+      unsigned as = a.size();
+      unsigned bs = b.size();
+      minmax(as, bs, m, x);
+      if (x == 1)
+      {
+         bool s = a.sign();
+         result = static_cast<double_limb_type>(*a.limbs()) + static_cast<double_limb_type>(*b.limbs());
+         result.sign(s);
+         return;
+      }
+      result.resize(x, x);
+      typename CppInt2::const_limb_pointer pa = a.limbs();
+      typename CppInt3::const_limb_pointer pb = b.limbs();
+      typename CppInt1::limb_pointer       pr = result.limbs();
+      //typename CppInt1::limb_pointer       pr_end = pr + m;
+
+      if (as < bs)
+         swap(pa, pb);
+      // First where a and b overlap:
+      unsigned      i = 0;
+      unsigned char carry = 0;
+      for (; i + 4 <= m; i += 4)
+      {
+         carry = ::boost::multiprecision::detail::addcarry_limb(carry, pa[i + 0], pb[i + 0], pr + i);
+         carry = ::boost::multiprecision::detail::addcarry_limb(carry, pa[i + 1], pb[i + 1], pr + i + 1);
+         carry = ::boost::multiprecision::detail::addcarry_limb(carry, pa[i + 2], pb[i + 2], pr + i + 2);
+         carry = ::boost::multiprecision::detail::addcarry_limb(carry, pa[i + 3], pb[i + 3], pr + i + 3);
+      }
+      for (; i < m; ++i)
+         carry = ::boost::multiprecision::detail::addcarry_limb(carry, pa[i], pb[i], pr + i);
+      for (; i < x && carry; ++i)
+         carry = ::boost::multiprecision::detail::addcarry_limb(carry, pa[i], 0, pr + i);
+      if (i == x && carry)
+      {
+         // We overflowed, need to add one more limb:
+         result.resize(x + 1, x + 1);
+         if (result.size() > x)
+            result.limbs()[x] = carry;
+      }
+      else
+         std::copy(pa + i, pa + x, pr + i);
+      result.normalize();
+      result.sign(a.sign());
+   }
+}
+
+#else
+#error 2
+template <class CppInt1, class CppInt2, class CppInt3>
+inline BOOST_MP_CXX14_CONSTEXPR void add_unsigned(CppInt1& result, const CppInt2& a, const CppInt3& b) BOOST_MP_NOEXCEPT_IF(is_non_throwing_cpp_int<CppInt1>::value)
+{
+   add_unsigned_constexpr(result, a, b);
+}
+
+#endif
+
+} } }  // namespaces
+
+
+#endif
+
+
---
+++ include/boost/multiprecision/cpp_int/intel_intrinsics.hpp
@@ -0,0 +1,67 @@
+///////////////////////////////////////////////////////////////
+//  Copyright 2020 John Maddock. Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef BOOST_MP_INTEL_INTRINSICS_HPP
+#define BOOST_MP_INTEL_INTRINSICS_HPP
+//
+// Select which actual implementation header to use:
+//
+#ifdef __has_include
+#if __has_include(<immintrin.h>)
+#define BOOST_MP_HAS_IMMINTRIN_H
+#endif
+#endif
+//
+// If this is GCC/clang, then check that the actual intrinsic exists:
+//
+#if defined(__has_builtin) && defined(__GNUC__)
+#if !__has_builtin(__builtin_ia32_addcarryx_u64) && defined(BOOST_MP_HAS_IMMINTRIN_H)
+#undef BOOST_MP_HAS_IMMINTRIN_H
+#endif
+#elif defined(BOOST_MP_HAS_IMMINTRIN_H) && defined(__GNUC__) && !(defined(BOOST_GCC) && (__GNUC__ >= 9))
+#undef BOOST_MP_HAS_IMMINTRIN_H
+#endif
+
+#if defined(__clang__) && (__clang__ < 9)
+// We appear to crash the compiler if we try to use these intrinsics?
+#undef BOOST_MP_HAS_IMMINTRIN_H
+#endif
+
+#ifdef BOOST_MP_HAS_IMMINTRIN_H
+
+#include <immintrin.h>
+
+#if defined(BOOST_HAS_INT128)
+
+namespace boost { namespace multiprecision { namespace detail {
+
+BOOST_MP_FORCEINLINE unsigned char addcarry_limb(unsigned char carry, limb_type a, limb_type b, limb_type* p_result)
+{
+#ifdef BOOST_INTEL
+   typedef unsigned __int64 cast_type;
+#else
+   typedef unsigned long long cast_type;
+#endif
+   return _addcarry_u64(carry, a, b, reinterpret_cast<cast_type*>(p_result));
+}
+
+}}} // namespace boost::multiprecision::detail
+
+#else
+
+namespace boost { namespace multiprecision { namespace detail {
+
+BOOST_MP_FORCEINLINE unsigned char addcarry_limb(unsigned char carry, limb_type a, limb_type b, limb_type* p_result)
+{
+   return _addcarry_u32(carry, a, b, reinterpret_cast<unsigned int*>(p_result));
+}
+
+}}} // namespace boost::multiprecision::detail
+
+#endif
+
+#endif
+
+#endif
