a78560ccb0325b16a7cc08ca5df800eb3fe39b72
jzmaddock <john@johnmaddock.co.uk>
2020-06-12 20:19:03 +0100

    Further improve max_digits_10 calculation.

--- include/boost/multiprecision/traits/max_digits10.hpp
+++ include/boost/multiprecision/traits/max_digits10.hpp
@@ -13,8 +13,24 @@ namespace detail {
    template <unsigned digits>
    struct calc_max_digits10
    {
-      BOOST_STATIC_CONSTANT(unsigned long long, base = static_cast<unsigned long long>(digits) * 301 + 1000);
-      BOOST_STATIC_CONSTANT(unsigned, value = static_cast<unsigned>(base / 1000 + 1));
+#ifndef BOOST_NO_CXX11_CONSTEXPR
+      static constexpr unsigned constexpr_ceil(double d)
+      {
+         return static_cast<unsigned>(d) == d ? static_cast<unsigned>(d) : static_cast<unsigned>(d) + 1;
+      }
+
+      static constexpr unsigned max_digits_10(unsigned d)
+      {
+         return constexpr_ceil(0.301029995663981195213738894724493026768189881462108541310 * d + 1);
+      }
+      static const unsigned value = max_digits_10(digits);
+#else
+      //
+      // This version works up to about 15K binary digits, then starts sporadically failing
+      // and giving values that are 1 too small.
+      //
+      BOOST_STATIC_CONSTANT(unsigned, value = 2 + ((static_cast<unsigned long long>(digits) * 1292913986) >> 32));
+#endif
    };
 
 
