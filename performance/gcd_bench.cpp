9b468e5e242a8d467f9dc588514d1c97368476b3
jzmaddock <john@johnmaddock.co.uk>
2020-07-16 20:11:47 +0100

    GCD: Update copyright declarations.

--- performance/gcd_bench.cpp
+++ performance/gcd_bench.cpp
@@ -1,3 +1,6 @@
+//  Copyright 2020 John Maddock. Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt
 
 #include <iostream>
 #include <benchmark/benchmark.h>
