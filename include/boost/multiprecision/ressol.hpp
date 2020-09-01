04bd0cd19af2a308abb490b2a68f0113ea951bb3
Mikhail Komarov <nemo@nil.foundation>
2020-09-01 16:06:59 +0300

    Minor changes #6

--- include/boost/multiprecision/ressol.hpp
+++ include/boost/multiprecision/ressol.hpp
@@ -1,8 +1,6 @@
 //---------------------------------------------------------------------------//
-// Copyright (c) 2018-2020 Nil Foundation AG
-// Copyright (c) 2018-2020 Mikhail Komarov <nemo@nilfoundation.org>
-// Copyright (c) 2018-2020 Alexey Moskvin
-// Copyright (c) 2018-2020 Pavel Kharitonov <ipavrus@nilfoundation.org>
+// Copyright (c) 2018-2020 Mikhail Komarov <nemo@nil.foundation>
+// Copyright (c) 2018-2020 Pavel Kharitonov <ipavrus@nil.foundation>
 //
 // Distributed under the Boost Software License, Version 1.0
 // See accompanying file LICENSE_1_0.txt or copy at
@@ -25,24 +23,24 @@ template <typename Backend>
 inline Backend eval_ressol(const Backend& a, const Backend& p)
 {
 
+   using default_ops::eval_add;
    using default_ops::eval_bit_set;
    using default_ops::eval_eq;
    using default_ops::eval_get_sign;
    using default_ops::eval_gt;
    using default_ops::eval_integer_modulus;
    using default_ops::eval_is_zero;
+   using default_ops::eval_left_shift;
    using default_ops::eval_lsb;
    using default_ops::eval_lt;
    using default_ops::eval_right_shift;
-   using default_ops::eval_left_shift;
    using default_ops::eval_subtract;
-   using default_ops::eval_add;
 
    typedef typename mpl::front<typename Backend::unsigned_types>::type ui_type;
 
-   Backend zero = ui_type(0u);
+   Backend zero   = ui_type(0u);
    Backend posone = ui_type(1u);
-   Backend two = ui_type(2u);
+   Backend two    = ui_type(2u);
    Backend negone, res;
 
    eval_subtract(negone, posone);
@@ -83,9 +81,9 @@ inline Backend eval_ressol(const Backend& a, const Backend& p)
    modular_adaptor<Backend> a_mod, res_mod;
 
    assign_components(a_mod, a, p);
-   
+
    if (eval_integer_modulus(p, 4) == 3)
-   {     
+   {
       Backend exp = p;
 
       eval_add(exp, posone);
@@ -95,7 +93,7 @@ inline Backend eval_ressol(const Backend& a, const Backend& p)
 
       return res;
    }
-   
+
    Backend p_negone = p, q = p;
 
    eval_add(p_negone, negone);
@@ -106,7 +104,7 @@ inline Backend eval_ressol(const Backend& a, const Backend& p)
    eval_subtract(q, posone);
    eval_right_shift(q, 1);
 
-   modular_adaptor <Backend> r_mod, n_mod = a_mod, r_sq_mod;
+   modular_adaptor<Backend> r_mod, n_mod = a_mod, r_sq_mod;
 
    eval_pow(r_mod, a_mod, q);
    eval_pow(r_sq_mod, r_mod, two);
@@ -132,7 +130,7 @@ inline Backend eval_ressol(const Backend& a, const Backend& p)
    eval_left_shift(q, 1);
    eval_add(q, posone);
 
-   modular_adaptor <Backend> z_mod, c_mod, q_mod;
+   modular_adaptor<Backend> z_mod, c_mod, q_mod;
 
    assign_components(z_mod, z, p);
    eval_pow(c_mod, z_mod, q);
@@ -141,13 +139,13 @@ inline Backend eval_ressol(const Backend& a, const Backend& p)
    while (eval_gt(n, 1))
    {
       Backend q;
-      size_t i = 0;
+      size_t  i = 0;
 
       q_mod = n_mod;
       q_mod.mod_data().adjust_regular(q, q_mod.base_data());
 
       while (!eval_eq(q, 1))
-      {  
+      {
          eval_pow(res_mod, q_mod, two);
          q_mod = res_mod;
          ++i;
