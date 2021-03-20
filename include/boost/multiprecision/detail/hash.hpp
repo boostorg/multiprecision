87455e044e6288db2bd1d0795369a2956dcaef6e
Matt Borland <matt@mattborland.com>
2021-03-20 11:26:36 +0300

    FIx arithmetic_tests CI failures

--- include/boost/multiprecision/detail/hash.hpp
+++ include/boost/multiprecision/detail/hash.hpp
@@ -7,7 +7,6 @@
 #define BOOST_MULTIPRECISION_DETAIL_HASH_HPP
 
 #include <cstddef>
-#include <functional>
 
 namespace boost { namespace multiprecision { namespace detail {
 
@@ -16,8 +15,8 @@ inline void hash_combine(std::size_t&) {}
 template <typename T, typename... Args>
 inline void hash_combine(std::size_t& seed, const T& v, Args... args) 
 {
-    std::hash<T> hasher;
-    seed ^= hasher(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+    // gmp types require explicit casting
+    seed = static_cast<std::size_t>(static_cast<T>(seed) ^ (v + 0x9e3779b9 + (seed<<6) + (seed>>2)));
     hash_combine(seed, args...);
 }
 
