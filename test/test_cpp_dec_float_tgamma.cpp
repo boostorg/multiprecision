57b4ae6fff87d2906fd0f8665b6aefce8c43381b
Christopher Kormanyos <ckormanyos@yahoo.com>
2021-05-20 17:09:13 +0200

    Repair new tgamma tests for legacy C++11

--- test/test_cpp_dec_float_tgamma.cpp
+++ test/test_cpp_dec_float_tgamma.cpp
@@ -25,39 +25,44 @@
 #include <boost/multiprecision/cpp_dec_float.hpp>
 #endif
 
-template<class T>
-void test()
+namespace local
 {
-   // N[Gamma[5/2], 120]
-   const T control_tgamma_2_and_half("1.32934038817913702047362561250585888709816209209179034616035584238968346344327413603121299255390849906217011771821192800");
+  template<typename T> constexpr T max(T a, T b) { return a > b ? a : b; }
 
-   // N[Gamma[500/2], 120]
-   const T control_tgamma_2_fifty   ("1.29314250436364309292832582080974738839793748706951226669917697084512949902204448379552716614841127978037140294127577317E490");
+  template<class T>
+  void test()
+  {
+     // N[Gamma[5/2], 120]
+     const T control_tgamma_2_and_half("1.32934038817913702047362561250585888709816209209179034616035584238968346344327413603121299255390849906217011771821192800");
 
-   const T tgamma_2_and_half = boost::math::tgamma(T(T(5)   / 2));
-   const T tgamma_2_fifty    = boost::math::tgamma(T(T(500) / 2));
+     // N[Gamma[500/2], 120]
+     const T control_tgamma_2_fifty   ("1.29314250436364309292832582080974738839793748706951226669917697084512949902204448379552716614841127978037140294127577317E490");
 
-   BOOST_CHECK_CLOSE_FRACTION(tgamma_2_and_half, control_tgamma_2_and_half, T(std::numeric_limits<T>::epsilon() * T(1.0E6)));
-   BOOST_CHECK_CLOSE_FRACTION(tgamma_2_fifty   , control_tgamma_2_fifty,    T(std::numeric_limits<T>::epsilon() * T(1.0E6)));
+     const T tgamma_2_and_half = boost::math::tgamma(T(T(5)   / 2));
+     const T tgamma_2_fifty    = boost::math::tgamma(T(T(500) / 2));
+
+     BOOST_CHECK_CLOSE_FRACTION(tgamma_2_and_half, control_tgamma_2_and_half, T(std::numeric_limits<T>::epsilon() * T(1.0E6)));
+     BOOST_CHECK_CLOSE_FRACTION(tgamma_2_fifty   , control_tgamma_2_fifty,    T(std::numeric_limits<T>::epsilon() * T(1.0E6)));
+  }
 }
 
 int main()
 {
   #if defined(TEST_CPP_BIN_FLOAT)
-   using big_float_type_016 = boost::multiprecision::number<boost::multiprecision::cpp_bin_float<(std::max)(std::numeric_limits<double>::digits + 1, 16)>>;
-   using big_float_type_035 = boost::multiprecision::number<boost::multiprecision::cpp_bin_float<(std::max)(std::numeric_limits<double>::digits + 1, 35)>>;
+   using big_float_type_016 = boost::multiprecision::number<boost::multiprecision::cpp_bin_float<(local::max)((int) (std::numeric_limits<double>::digits + 1), 16)>>;
+   using big_float_type_035 = boost::multiprecision::number<boost::multiprecision::cpp_bin_float<(local::max)((int) (std::numeric_limits<double>::digits + 1), 35)>>;
    using big_float_type_105 = boost::multiprecision::number<boost::multiprecision::cpp_bin_float<105>>;
   #endif
 
   #if defined(TEST_CPP_DEC_FLOAT)
-   using big_float_type_016 = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<(std::max)(std::numeric_limits<double>::digits + 1, 16)>>;
-   using big_float_type_035 = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<(std::max)(std::numeric_limits<double>::digits + 1, 35)>>;
+   using big_float_type_016 = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<(local::max)((int) (std::numeric_limits<double>::digits + 1), 16)>>;
+   using big_float_type_035 = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<(local::max)((int) (std::numeric_limits<double>::digits + 1), 35)>>;
    using big_float_type_105 = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<105>>;
   #endif
 
-   test<big_float_type_016>();
-   test<big_float_type_035>();
-   test<big_float_type_105>();
+   local::test<big_float_type_016>();
+   local::test<big_float_type_035>();
+   local::test<big_float_type_105>();
 
    return boost::report_errors();
 }
