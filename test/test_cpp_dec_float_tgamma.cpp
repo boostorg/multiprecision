14ca6f139b0c25aa6bacf96f07e9e8e3ce3a586c
Christopher Kormanyos <ckormanyos@yahoo.com>
2021-05-20 15:22:21 +0200

    [ci skip] Improved draft potential tgamma tests

--- test/test_cpp_dec_float_tgamma.cpp
+++ test/test_cpp_dec_float_tgamma.cpp
@@ -4,15 +4,26 @@
 //  Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt
 //
-// This work is based on an earlier work:
-// "Algorithm 910: A Portable C++ Multiple-Precision System for Special-Function Calculations",
-// in ACM TOMS, {VOL 37, ISSUE 4, (February 2011)} (C) ACM, 2011. http://doi.acm.org/10.1145/1916461.1916469
+
+#if !defined(TEST_CPP_BIN_FLOAT) && !defined(TEST_CPP_DEC_FLOAT)
+#define TEST_CPP_DEC_FLOAT
+#endif
+
+#if defined(TEST_CPP_BIN_FLOAT) && defined(TEST_CPP_DEC_FLOAT)
+#error Error: only one of either TEST_CPP_BIN_FLOAT or TEST_CPP_DEC_FLOAT can be defined at the same time.
+#endif
 
 #include <boost/detail/lightweight_test.hpp>
 #include <boost/math/special_functions/gamma.hpp>
 #include "test.hpp"
 
+#if defined(TEST_CPP_BIN_FLOAT)
+#include <boost/multiprecision/cpp_bin_float.hpp>
+#endif
+
+#if defined(TEST_CPP_DEC_FLOAT)
 #include <boost/multiprecision/cpp_dec_float.hpp>
+#endif
 
 template<class T>
 void test()
@@ -26,17 +37,25 @@ void test()
    const T tgamma_2_and_half = boost::math::tgamma(T(T(5)   / 2));
    const T tgamma_2_fifty    = boost::math::tgamma(T(T(500) / 2));
 
-   BOOST_CHECK_CLOSE_FRACTION(tgamma_2_and_half, control_tgamma_2_and_half, T(std::numeric_limits<T>::epsilon() * 100));
-   BOOST_CHECK_CLOSE_FRACTION(tgamma_2_fifty   , control_tgamma_2_fifty,    T(std::numeric_limits<T>::epsilon() * 100));
+   BOOST_CHECK_CLOSE_FRACTION(tgamma_2_and_half, control_tgamma_2_and_half, T(std::numeric_limits<T>::epsilon() * T(1.0E6)));
+   BOOST_CHECK_CLOSE_FRACTION(tgamma_2_fifty   , control_tgamma_2_fifty,    T(std::numeric_limits<T>::epsilon() * T(1.0E6)));
 }
 
 int main()
 {
-   using big_float_type_010 = boost::multiprecision::number<boost::multiprecision::cpp_dec_float< 10>>;
-   using big_float_type_035 = boost::multiprecision::number<boost::multiprecision::cpp_dec_float< 35>>;
-   using big_float_type_105 = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<105>>;
+  #if defined(TEST_CPP_BIN_FLOAT)
+   using big_float_type_016 = boost::multiprecision::number<boost::multiprecision::cpp_bin_float<(std::max)(std::numeric_limits<double>::digits + 1, 16)>>;
+   using big_float_type_035 = boost::multiprecision::number<boost::multiprecision::cpp_bin_float<(std::max)(std::numeric_limits<double>::digits + 1, 35)>>;
+   using big_float_type_105 = boost::multiprecision::number<boost::multiprecision::cpp_bin_float<105>>;
+  #endif
 
-   test<big_float_type_010>();
+  #if defined(TEST_CPP_DEC_FLOAT)
+   using big_float_type_016 = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<(std::max)(std::numeric_limits<double>::digits + 1, 16)>>;
+   using big_float_type_035 = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<(std::max)(std::numeric_limits<double>::digits + 1, 35)>>;
+   using big_float_type_105 = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<105>>;
+  #endif
+
+   test<big_float_type_016>();
    test<big_float_type_035>();
    test<big_float_type_105>();
 
