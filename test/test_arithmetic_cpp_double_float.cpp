e7836a949322a9fbdad439bceccc69fd3186472c
Christopher Kormanyos <ckormanyos@yahoo.com>
2021-06-20 10:05:04 +0200

    Basic test add/sub/mul/div only add/mul pass

--- test/test_arithmetic_cpp_double_float.cpp
+++ test/test_arithmetic_cpp_double_float.cpp
@@ -9,6 +9,9 @@
 
 namespace local
 {
+  using naked_double_float_type = boost::multiprecision::backends::cpp_double_float<double>;
+  using control_float_type      = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<32U>, boost::multiprecision::et_off>;
+
   std::mt19937                                                         engine_man;
   std::ranlux24_base                                                   engine_sgn;
   std::linear_congruential_engine<std::uint32_t, 48271, 0, 2147483647> engine_dec_pt;
@@ -89,45 +92,163 @@ namespace local
       ++pos;
     }
   }
-}
 
-using naked_double_float_type = boost::multiprecision::backends::cpp_double_float<double>;
-using control_float_type      = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<34U>, boost::multiprecision::et_off>;
-
-int main()
-{
-  for(unsigned i = 0U; i < 16U; ++i)
+  bool test_add(const unsigned count)
   {
-    std::string str_a;
-    std::string str_b;
+    bool result_is_ok = true;
 
-    local::get_random_fixed_string<33U>(str_a);
-    local::get_random_fixed_string<33U>(str_b);
-
-    const naked_double_float_type df_a  (str_a);
-    const naked_double_float_type df_b  (str_b);
-
-    const control_float_type      ctrl_a(str_a);
-    const control_float_type      ctrl_b(str_b);
-
-    naked_double_float_type df_c    = df_a   + df_b;
-    control_float_type      ctrl_c  = ctrl_a + ctrl_b;
-
-    std::cout << "df_c  : " << std::setprecision(34) << df_c   << std::endl;
-    std::cout << "ctrl_c: " << std::setprecision(34) << ctrl_c << std::endl;
-
-    bool result_is_ok;
-
+    for(unsigned i = 0U; i < count; ++i)
     {
+      std::string str_a;
+      std::string str_b;
+
+      local::get_random_fixed_string<33U>(str_a);
+      local::get_random_fixed_string<33U>(str_b);
+
+      const naked_double_float_type df_a  (str_a);
+      const naked_double_float_type df_b  (str_b);
+
+      const control_float_type      ctrl_a(str_a);
+      const control_float_type      ctrl_b(str_b);
+
+      naked_double_float_type df_c    = df_a   + df_b;
+      control_float_type      ctrl_c  = ctrl_a + ctrl_b;
+
       std::stringstream strm;
 
-      strm << std::setprecision(34) << df_c;
+      strm << std::setprecision(33) << df_c;
 
       const std::string str_df_c = strm.str();
 
-      result_is_ok = (fabs(1 - fabs(ctrl_c / control_float_type(str_df_c))) < std::numeric_limits<control_float_type>::epsilon() * 1000000UL);
+      const bool b_ok =
+        (fabs(1 - fabs(ctrl_c / control_float_type(str_df_c))) < std::numeric_limits<control_float_type>::epsilon() * 10000UL);
+
+      result_is_ok &= b_ok;
     }
 
-    std::cout << "result_is_ok: " << std::boolalpha << result_is_ok << std::endl;
+    return result_is_ok;
   }
+
+  bool test_sub(const unsigned count)
+  {
+    bool result_is_ok = true;
+
+    for(unsigned i = 0U; i < count; ++i)
+    {
+      std::string str_a;
+      std::string str_b;
+
+      local::get_random_fixed_string<33U>(str_a);
+      local::get_random_fixed_string<33U>(str_b);
+
+      const naked_double_float_type df_a  (str_a);
+      const naked_double_float_type df_b  (str_b);
+
+      const control_float_type      ctrl_a(str_a);
+      const control_float_type      ctrl_b(str_b);
+
+      naked_double_float_type df_c    = df_a   - df_b;
+      control_float_type      ctrl_c  = ctrl_a - ctrl_b;
+
+      std::stringstream strm;
+
+      strm << std::setprecision(33) << df_c;
+
+      const std::string str_df_c = strm.str();
+
+      const bool b_ok =
+        (fabs(1 - fabs(ctrl_c / control_float_type(str_df_c))) < std::numeric_limits<control_float_type>::epsilon() * 10000UL);
+
+      result_is_ok &= b_ok;
+    }
+
+    return result_is_ok;
+  }
+
+  bool test_mul(const unsigned count)
+  {
+    bool result_is_ok = true;
+
+    for(unsigned i = 0U; i < count; ++i)
+    {
+      std::string str_a;
+      std::string str_b;
+
+      local::get_random_fixed_string<33U>(str_a);
+      local::get_random_fixed_string<33U>(str_b);
+
+      const naked_double_float_type df_a  (str_a);
+      const naked_double_float_type df_b  (str_b);
+
+      const control_float_type      ctrl_a(str_a);
+      const control_float_type      ctrl_b(str_b);
+
+      naked_double_float_type df_c    = df_a   * df_b;
+      control_float_type      ctrl_c  = ctrl_a * ctrl_b;
+
+      std::stringstream strm;
+
+      strm << std::setprecision(33) << df_c;
+
+      const std::string str_df_c = strm.str();
+
+      const bool b_ok =
+        (fabs(1 - fabs(ctrl_c / control_float_type(str_df_c))) < std::numeric_limits<control_float_type>::epsilon() * 10000UL);
+
+      result_is_ok &= b_ok;
+    }
+
+    return result_is_ok;
+  }
+
+  bool test_div(const unsigned count)
+  {
+    bool result_is_ok = true;
+
+    for(unsigned i = 0U; i < count; ++i)
+    {
+      std::string str_a;
+      std::string str_b;
+
+      local::get_random_fixed_string<33U>(str_a);
+      local::get_random_fixed_string<33U>(str_b);
+
+      const naked_double_float_type df_a  (str_a);
+      const naked_double_float_type df_b  (str_b);
+
+      const control_float_type      ctrl_a(str_a);
+      const control_float_type      ctrl_b(str_b);
+
+      naked_double_float_type df_c    = df_a   / df_b;
+      control_float_type      ctrl_c  = ctrl_a / ctrl_b;
+
+      std::stringstream strm;
+
+      strm << std::setprecision(33) << df_c;
+
+      const std::string str_df_c = strm.str();
+
+      const bool b_ok =
+        (fabs(1 - fabs(ctrl_c / control_float_type(str_df_c))) < std::numeric_limits<control_float_type>::epsilon() * 10000UL);
+
+      result_is_ok &= b_ok;
+    }
+
+    return result_is_ok;
+  }
+}
+
+int main()
+{
+  const bool result_add_is_ok = local::test_add(1024U); std::cout << "result_add_is_ok: " << std::boolalpha << result_add_is_ok << std::endl;
+  const bool result_sub_is_ok = local::test_sub(1024U); std::cout << "result_sub_is_ok: " << std::boolalpha << result_sub_is_ok << std::endl;
+  const bool result_mul_is_ok = local::test_mul(1024U); std::cout << "result_mul_is_ok: " << std::boolalpha << result_mul_is_ok << std::endl;
+  const bool result_div_is_ok = local::test_div(1024U); std::cout << "result_div_is_ok: " << std::boolalpha << result_div_is_ok << std::endl;
+
+  const bool result_all_is_ok = (   result_add_is_ok
+                                 /*&& result_sub_is_ok*/
+                                 && result_mul_is_ok
+                                 /*&& result_div_is_ok*/);
+
+  return (result_all_is_ok ? 0 : -1);
 }
