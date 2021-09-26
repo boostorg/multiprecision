6c07fa436a2bd06f02dbd2bc717e5b97691d80bf
Christopher Kormanyos <ckormanyos@yahoo.com>
2021-09-26 13:08:17 +0200

    Repait typo and add test depth

--- test/git_issue_368.cpp
+++ test/git_issue_368.cpp
@@ -12,17 +12,51 @@
 
 namespace local
 {
+   std::minstd_rand0 eng(static_cast<std::minstd_rand0::result_type>(std::clock()));
+
+   std::uniform_int_distribution<unsigned> dist_sign
+   (
+      0U,
+      1U
+   );
+
+  template<typename DecimalType>
+  void test_floor_rounding()
+  {
+    using decimal_type = DecimalType;
+
+    bool result_is_ok = true;
+
+    for(std::int32_t lo_index = 101U; lo_index < 1010U; lo_index += 7U)
+    {
+      for(std::int32_t hi_index = 10001U; hi_index < 100010U; hi_index += 17U)
+      {
+        const bool lo_is_neg = ((dist_sign(eng) % 2U) == 0U);
+        const bool hi_is_neg = ((dist_sign(eng) % 2U) == 0U);
+
+        const std::int32_t lo = ((lo_is_neg == false) ? lo_index : -lo_index);
+        const std::int32_t hi = ((hi_is_neg == false) ? hi_index : -hi_index);
+
+        const std::int32_t lo_hi = lo * hi;
+
+        const decimal_type a = decimal_type { lo_hi } / decimal_type { lo };
+        const decimal_type b = floor(a);
+
+        result_is_ok &= (static_cast<std::int32_t>(a) == b);
+
+        if(result_is_ok == false)
+        {
+          break;
+        }
+      }
+    }
+
+    BOOST_CHECK_EQUAL(result_is_ok, true);
+  }
+
   template<typename DecimalType>
   void test_ceil_rounding()
   {
-    static std::minstd_rand0 eng(static_cast<std::minstd_rand0::result_type>(std::clock()));
-
-    static const std::uniform_int_distribution<unsigned> dist_sign
-    (
-       0U,
-       1U
-    );
-
     using decimal_type = DecimalType;
 
     bool result_is_ok = true;
@@ -63,6 +97,7 @@ int main(int, char**)
     using decimal = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<10>,
                                                   boost::multiprecision::et_off>;
 
+    local::test_floor_rounding<decimal>();
     local::test_ceil_rounding<decimal>();
   }
 
@@ -70,6 +105,7 @@ int main(int, char**)
     using decimal = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<12>,
                                                   boost::multiprecision::et_off>;
 
+    local::test_floor_rounding<decimal>();
     local::test_ceil_rounding<decimal>();
   }
 
