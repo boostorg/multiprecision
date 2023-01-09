8741dc8d242314c4dd1ed7adca8ac04b5eca1804
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-01-09 08:58:19 +0100

    Split depth of test_arithmetic_df to multi-file

--- test/test_arithmetic_df128.cpp
+++ test/test_arithmetic_df128.cpp
@@ -9,15 +9,19 @@
 #define _SCL_SECURE_NO_WARNINGS
 #endif
 
+#ifdef BOOST_HAS_FLOAT128
 #include <boost/multiprecision/cpp_double_fp.hpp>
 
 #include "test_arithmetic.hpp"
+#endif
 
 int main()
 {
-   using boost::multiprecision::cpp_double_double;
+#ifdef BOOST_HAS_FLOAT128
+   using boost::multiprecision::cpp_double_float128;
 
-   test<cpp_double_double>();
+   test<cpp_double_float128>();
 
    return boost::report_errors();
+#endif
 }
