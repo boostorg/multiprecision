09386741e25fd1173977bdee86bfca7538dc991b
jzmaddock <john@johnmaddock.co.uk>
2023-01-14 09:22:36 +0000

    Correct test configuration. Add copyright and licence notices to new files.

--- test/logged_adaptor_output_test.cpp
+++ test/logged_adaptor_output_test.cpp
@@ -1,3 +1,8 @@
+///////////////////////////////////////////////////////////////
+//  Copyright 2023 John Maddock. Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt
+
 #include <iostream>
 #include <iomanip>
 #include <boost/multiprecision/fwd.hpp>
