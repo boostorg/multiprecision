d76f19bbe3245b2c6f414276267b67ec5d8f23b1
Matt Borland <matt@mattborland.com>
2024-05-07 12:20:19 +0200

    Add constexpr annotations

--- include/boost/multiprecision/complex.hpp
+++ include/boost/multiprecision/complex.hpp
@@ -26,38 +26,38 @@ private:
     T imag_;
 
 public:
-    complex() noexcept = default;
-    complex(T real) noexcept : real_ {real}, imag_ {T{0}} {}
-    complex(T real, T imag) noexcept : real_ {real}, imag_ {imag} {}
-    complex& operator=(const complex& rhs)
+    BOOST_MP_CXX14_CONSTEXPR complex() noexcept = default;
+    BOOST_MP_CXX14_CONSTEXPR complex(T real) noexcept : real_ {real}, imag_ {T{0}} {}
+    BOOST_MP_CXX14_CONSTEXPR complex(T real, T imag) noexcept : real_ {real}, imag_ {imag} {}
+    BOOST_MP_CXX14_CONSTEXPR complex& operator=(const complex& rhs)
     {
         real_ = rhs.real_;
         imag_ = rhs.imag_;
         return *this;
     }
 
-    T real() const noexcept { return real_; }
-    T imag() const noexcept { return imag_; }
+    BOOST_MP_CXX14_CONSTEXPR T real() const noexcept { return real_; }
+    BOOST_MP_CXX14_CONSTEXPR T imag() const noexcept { return imag_; }
 
-    complex operator+() const { return *this; }
-    complex operator-() const { return {-real_, -imag_}; }
+    BOOST_MP_CXX14_CONSTEXPR complex operator+() const { return *this; }
+    BOOST_MP_CXX14_CONSTEXPR complex operator-() const { return {-real_, -imag_}; }
 
-    complex operator+(const complex& rhs) noexcept
+    BOOST_MP_CXX14_CONSTEXPR complex operator+(const complex& rhs) noexcept
     {
         return {real_ + rhs.real_, imag_ + rhs.imag_};
     }
 
-    complex operator-(const complex& rhs) noexcept
+    BOOST_MP_CXX14_CONSTEXPR complex operator-(const complex& rhs) noexcept
     {
         return {real_ - rhs.real_, imag_ - rhs.imag_};
     }
 
-    complex operator*(const complex& rhs) noexcept
+    BOOST_MP_CXX14_CONSTEXPR complex operator*(const complex& rhs) noexcept
     {
         return {real_ * rhs.real_ - imag_ * rhs.imag_, imag_ * rhs.real_ + real_ * rhs.imag_};
     }
 
-    complex operator/(const complex& rhs) noexcept
+    BOOST_MP_CXX14_CONSTEXPR complex operator/(const complex& rhs) noexcept
     {
         const T divisor = rhs.real_ * rhs.real_ + rhs.imag_ * rhs.imag_;
         const T real_part = (real_ * rhs.real_ + imag_ * rhs.imag_) / divisor;
@@ -65,46 +65,46 @@ public:
         return {real_part, imag_part};
     }
 
-    complex& operator+=(const complex& rhs) noexcept
+    BOOST_MP_CXX14_CONSTEXPR complex& operator+=(const complex& rhs) noexcept
     {
         *this = *this + rhs;
         return *this;
     }
 
-    complex& operator-=(const complex& rhs) noexcept
+    BOOST_MP_CXX14_CONSTEXPR complex& operator-=(const complex& rhs) noexcept
     {
         *this = *this - rhs;
         return *this;
     }
 
-    complex& operator*=(const complex& rhs) noexcept
+    BOOST_MP_CXX14_CONSTEXPR complex& operator*=(const complex& rhs) noexcept
     {
         *this = *this * rhs;
         return *this;
     }
 
-    complex& operator/=(const complex& rhs) noexcept
+    BOOST_MP_CXX14_CONSTEXPR complex& operator/=(const complex& rhs) noexcept
     {
         *this = *this / rhs;
         return *this;
     }
 
-    bool operator==(const complex& rhs) noexcept
+    BOOST_MP_CXX14_CONSTEXPR bool operator==(const complex& rhs) noexcept
     {
         return real_ == rhs.real_ && imag_ == rhs.imag_;
     }
 
-    bool operator!=(const complex& rhs) noexcept
+    BOOST_MP_CXX14_CONSTEXPR bool operator!=(const complex& rhs) noexcept
     {
         return !(*this == rhs);
     }
 
-    bool operator==(const T& rhs) noexcept
+    BOOST_MP_CXX14_CONSTEXPR bool operator==(const T& rhs) noexcept
     {
         return real_ == rhs && imag_ == T{0};
     }
 
-    bool operator!=(const T& rhs) noexcept
+    BOOST_MP_CXX14_CONSTEXPR bool operator!=(const T& rhs) noexcept
     {
         return !(*this == rhs);
     }
@@ -172,27 +172,27 @@ public:
 };
 
 template <typename T, expression_template_option ET>
-inline complex<boost::multiprecision::number<T, ET>> polar(const boost::multiprecision::number<T, ET>& rho,
-                                                           const boost::multiprecision::number<T, ET>& theta) noexcept
+inline BOOST_MP_CXX14_CONSTEXPR complex<boost::multiprecision::number<T, ET>> polar(const boost::multiprecision::number<T, ET>& rho,
+                                                                                    const boost::multiprecision::number<T, ET>& theta) noexcept
 {
     BOOST_MP_ASSERT_MSG(rho >= T{0}, "Rho must be positive");
     return polar(rho, theta);
 }
 
 template <typename T, expression_template_option ET>
-inline boost::multiprecision::number<T, ET> real(const complex<boost::multiprecision::number<T, ET>>& z) noexcept
+inline BOOST_MP_CXX14_CONSTEXPR boost::multiprecision::number<T, ET> real(const complex<boost::multiprecision::number<T, ET>>& z) noexcept
 {
     return z.real();
 }
 
 template <typename T, expression_template_option ET>
-inline boost::multiprecision::number<T, ET> imag(const complex<boost::multiprecision::number<T, ET>>& z) noexcept
+inline BOOST_MP_CXX14_CONSTEXPR boost::multiprecision::number<T, ET> imag(const complex<boost::multiprecision::number<T, ET>>& z) noexcept
 {
     return z.imag();
 }
 
 template <typename T, expression_template_option ET>
-inline boost::multiprecision::number<T, ET> abs(const complex<boost::multiprecision::number<T, ET>>& z) noexcept
+inline BOOST_MP_CXX14_CONSTEXPR boost::multiprecision::number<T, ET> abs(const complex<boost::multiprecision::number<T, ET>>& z) noexcept
 {
     return hypot(z.real(), z.imag());
 }
@@ -205,6 +205,6 @@ namespace std {
 template <typename T, boost::multiprecision::expression_template_option ET>
 class complex<boost::multiprecision::number<T, ET>> : public boost::multiprecision::complex<boost::multiprecision::number<T, ET>> {};
 
-}
+} // namespace std
 
 #endif //BOOST_MP_COMPLEX_HPP
