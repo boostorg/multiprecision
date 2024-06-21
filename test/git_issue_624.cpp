891e2c39808b40f4f39d14b7e3280683186935cb
jzmaddock <john@johnmaddock.co.uk>
2024-06-21 17:11:30 +0100

    [Cosmetic] Add copyright/licence. [CI SKIP]

--- test/git_issue_624.cpp
+++ test/git_issue_624.cpp
@@ -1,3 +1,7 @@
+///////////////////////////////////////////////////////////////
+//  Copyright 2024 John Maddock. Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt
 
 #include <iostream>
 #include <boost/multiprecision/cpp_int.hpp>
