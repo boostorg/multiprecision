f4e01b8406ffdb1adcb66bddf049a65f5fc49265
ckormanyos <ckormanyos@yahoo.com>
2025-01-07 19:00:02 +0100

    Restore 128-bit FP limits

--- include/boost/multiprecision/cpp_df_qf/cpp_df_qf_detail_ccmath_limits.hpp
+++ include/boost/multiprecision/cpp_df_qf/cpp_df_qf_detail_ccmath_limits.hpp
@@ -26,11 +26,7 @@ template <class FloatingPointType>
 struct numeric_limits<FloatingPointType,
                       typename std::enable_if<(   std::is_same<FloatingPointType, float>::value
                                                || std::is_same<FloatingPointType, double>::value
-                                               || std::is_same<FloatingPointType, long double>::value
-#if defined(BOOST_MP_CPP_DOUBLE_FP_HAS_FLOAT128)
-                                               || std::is_same<FloatingPointType, ::boost::float128_type>::value
-#endif
-                                               )>::type>
+                                               || std::is_same<FloatingPointType, long double>::value)>::type>
 {
 private:
    using float_type = FloatingPointType;
@@ -72,6 +68,124 @@ public:
    static constexpr auto  quiet_NaN    () noexcept -> float_type { return  std::numeric_limits<float_type>::quiet_NaN    (); }
    static constexpr auto  signaling_NaN() noexcept -> float_type { return  std::numeric_limits<float_type>::signaling_NaN(); }
 };
+
+#if defined(BOOST_MP_CPP_DOUBLE_FP_HAS_FLOAT128)
+template <class FloatingPointType>
+struct numeric_limits<FloatingPointType,
+                      typename std::enable_if<std::is_same<FloatingPointType, ::boost::float128_type>::value>::type>
+{
+private:
+   using self_type = ::boost::float128_type;
+
+public:
+   static constexpr bool                    is_specialized                = true;
+   static constexpr bool                    is_signed                     = true;
+   static constexpr bool                    is_integer                    = false;
+   static constexpr bool                    is_exact                      = false;
+   static constexpr bool                    is_bounded                    = true;
+   static constexpr bool                    is_modulo                     = false;
+   static constexpr bool                    is_iec559                     = true;
+   static constexpr std::float_denorm_style has_denorm                    = std::denorm_present;
+   static constexpr bool                    has_infinity                  = true;
+   static constexpr bool                    has_quiet_NaN                 = true;
+   static constexpr bool                    has_signaling_NaN             = false;
+   static constexpr bool                    has_denorm_loss               = true;
+   static constexpr bool                    traps                         = false;
+   static constexpr bool                    tinyness_before               = false;
+   static constexpr std::float_round_style  round_style                   = std::round_to_nearest;
+
+   static constexpr int radix                          = 2;
+   static constexpr int digits                         = 113;
+   static constexpr int digits10                       = 33;
+   static constexpr int max_digits10                   = 36;
+
+   static constexpr int max_exponent                   = 16384;
+   static constexpr int min_exponent                   = -16381;
+   static constexpr int max_exponent10                 = static_cast<int>(static_cast<long long>(static_cast<long long>(max_exponent) * 301LL) / 1000LL);
+   static constexpr int min_exponent10                 = static_cast<int>(static_cast<long long>(static_cast<long long>(min_exponent) * 301LL) / 1000LL);
+
+   static constexpr self_type (min)() noexcept
+   {
+     return   static_cast<self_type>(1)
+            * static_cast<self_type>(DBL_MIN)
+            * static_cast<self_type>(DBL_MIN)
+            * static_cast<self_type>(DBL_MIN)
+            * static_cast<self_type>(DBL_MIN)
+            * static_cast<self_type>(DBL_MIN)
+            * static_cast<self_type>(DBL_MIN)
+            * static_cast<self_type>(DBL_MIN)
+            * static_cast<self_type>(DBL_MIN)
+            * static_cast<self_type>(DBL_MIN)
+            * static_cast<self_type>(DBL_MIN)
+            * static_cast<self_type>(DBL_MIN)
+            * static_cast<self_type>(DBL_MIN)
+            * static_cast<self_type>(DBL_MIN)
+            * static_cast<self_type>(DBL_MIN)
+            * static_cast<self_type>(DBL_MIN)
+            * static_cast<self_type>(DBL_MIN) / 1073741824;
+   }
+
+   static constexpr self_type (max)() noexcept
+   {
+      // This has one bit set only.
+      constexpr double dbl_mult = 8.9884656743115795386e+307;
+
+      return   (static_cast<self_type>(1) - 9.62964972193617926527988971292463659e-35) // This now has all bits sets to 1
+             *  static_cast<self_type>(dbl_mult)
+             *  static_cast<self_type>(dbl_mult)
+             *  static_cast<self_type>(dbl_mult)
+             *  static_cast<self_type>(dbl_mult)
+             *  static_cast<self_type>(dbl_mult)
+             *  static_cast<self_type>(dbl_mult)
+             *  static_cast<self_type>(dbl_mult)
+             *  static_cast<self_type>(dbl_mult)
+             *  static_cast<self_type>(dbl_mult)
+             *  static_cast<self_type>(dbl_mult)
+             *  static_cast<self_type>(dbl_mult)
+             *  static_cast<self_type>(dbl_mult)
+             *  static_cast<self_type>(dbl_mult)
+             *  static_cast<self_type>(dbl_mult)
+             *  static_cast<self_type>(dbl_mult)
+             *  static_cast<self_type>(dbl_mult) * 65536;
+   }
+
+   static constexpr self_type lowest() noexcept { return -(max)(); }
+
+   static constexpr self_type epsilon()
+   {
+     // This double value has only one bit set and so is exact.
+     return 1.92592994438723585305597794258492732e-34;
+   }
+
+   static constexpr self_type round_error() noexcept { return 0.5; }
+
+   static constexpr self_type denorm_min() noexcept
+   {
+      return   static_cast<self_type>(1)
+             * static_cast<self_type>(DBL_MIN)
+             * static_cast<self_type>(DBL_MIN)
+             * static_cast<self_type>(DBL_MIN)
+             * static_cast<self_type>(DBL_MIN)
+             * static_cast<self_type>(DBL_MIN)
+             * static_cast<self_type>(DBL_MIN)
+             * static_cast<self_type>(DBL_MIN)
+             * static_cast<self_type>(DBL_MIN)
+             * static_cast<self_type>(DBL_MIN)
+             * static_cast<self_type>(DBL_MIN)
+             * static_cast<self_type>(DBL_MIN)
+             * static_cast<self_type>(DBL_MIN)
+             * static_cast<self_type>(DBL_MIN)
+             * static_cast<self_type>(DBL_MIN)
+             * static_cast<self_type>(DBL_MIN)
+             * static_cast<self_type>(DBL_MIN)
+             / 5.5751862996326557854e+42;
+   }
+
+   static constexpr self_type infinity     () noexcept { return HUGE_VAL; }
+   static constexpr self_type quiet_NaN    () noexcept { return NAN; }
+   static constexpr self_type signaling_NaN() noexcept { return 0; }
+};
+#endif
 
 } } } } } // namespace boost::backends::cpp_df_qf_detail::ccmath
 
