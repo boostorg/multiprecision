2c0d0cdc00d1038ff26764acde1c6205a1972064
Marco Ribeiro <speaktomarco@gmail.com>
2025-02-14 00:11:13 -0300

    Add tests for dynamic allocated cpp_int

---
+++ test/constexpr_test_dynamic_cpp_int_1.cpp
@@ -0,0 +1,14 @@
+//  (C) Copyright John Maddock 2025.
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/config.hpp>
+#undef BOOST_HAS_INT128
+
+#include "constexpr_test_dynamic_cpp_int.hpp"
+
+
+int main(){
+  return run_constexpr_cpp_int_tests();
+}
