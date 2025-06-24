ff0b5ff588fc3105791cb8e4f0c4b776a649cb23
Matt Borland <matt@mattborland.com>
2025-06-24 11:12:58 -0400

    Add header data

--- test/reduced_cpp_double_fp.cpp
+++ test/reduced_cpp_double_fp.cpp
@@ -1,3 +1,8 @@
+//  Copyright John Maddock 2007.
+//  Copyright Matt Borland 2023.
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <boost/core/lightweight_test.hpp>
 #include <boost/multiprecision/cpp_double_fp.hpp>
