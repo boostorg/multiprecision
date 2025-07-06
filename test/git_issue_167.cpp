4f4783636142e9872e871d598fd321f1ea500db5
ckormanyos <ckormanyos@yahoo.com>
2025-07-06 14:07:23 +0200

    Annotate and slight rework some tests for cover

--- test/git_issue_167.cpp
+++ test/git_issue_167.cpp
@@ -1,23 +1,40 @@
 ///////////////////////////////////////////////////////////////////////////////
-//  Copyright 2019 John Maddock. Distributed under the Boost
+//  Copyright 2019 - 2025 John Maddock.
+//  Copyright 2025 Christopher Kormanyos.
+//  Distributed under the Boost
 //  Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#include <test.hpp>
+
 #include <boost/multiprecision/cpp_dec_float.hpp>
-#include "test.hpp"
+
+#include <locale>
+#include <string>
 
 int main()
 {
-   try{
-      std::locale::global(std::locale("en-US"));
-      boost::multiprecision::cpp_dec_float_50 d("1234.56");
-      std::string s = d.str();
+   std::string str { };
 
-      BOOST_CHECK_EQUAL(s, "1234.56");
+   try
+   {
+      using local_dec_float_type = boost::multiprecision::cpp_dec_float_50;
+
+      std::locale::global(std::locale("en-US"));
+      local_dec_float_type d("1234.56");
+      str = d.str();
+
+      BOOST_CHECK_EQUAL(str, "1234.56");
+
+      d = local_dec_float_type("123.456");
+      str = d.str();
+
+      BOOST_CHECK_EQUAL(str, "123.456");
    }
    catch(const std::runtime_error&){}  // No en-US locale
 
+   BOOST_CHECK(!str.empty());
+   BOOST_CHECK(str.find('.') != std::string::npos);
+
    return boost::report_errors();
 }
-
-
