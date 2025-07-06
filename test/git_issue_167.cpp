14f4212c02121abc7706e3ec9bff64cf26448266
ckormanyos <ckormanyos@yahoo.com>
2025-07-06 19:00:22 +0200

    Repair and cover test for issue167

--- test/git_issue_167.cpp
+++ test/git_issue_167.cpp
@@ -20,21 +20,39 @@ int main()
    {
       using local_dec_float_type = boost::multiprecision::cpp_dec_float_50;
 
-      std::locale::global(std::locale("en-US"));
-      local_dec_float_type d("1234.56");
-      str = d.str();
+      // Test a string with a decimal point.
+      local_dec_float_type d_first("1234.56");
+      str = d_first.str();
+      BOOST_CHECK(str == "1234.56");
 
-      BOOST_CHECK_EQUAL(str, "1234.56");
+      // Set a new local locale.
+      #if (defined(__GNUC__) && defined(__x86_64__))
+      // We really want to pick up these lines on GHA coverage runners.
+      const char* p_str_loc = std::setlocale(LC_ALL, "C.UTF-8");
+      #else
+      const char* p_str_loc = std::setlocale(LC_ALL, "en_US.UTF-8");
+      #endif
 
-      d = local_dec_float_type("123.456");
-      str = d.str();
+      std::cout << "A local locale was set: " << p_str_loc << std::endl;
 
-      BOOST_CHECK_EQUAL(str, "123.456");
+      {
+         std::stringstream strm { };
+
+         strm << p_str_loc;
+
+         // Check the new local locale.
+         BOOST_CHECK(strm.str().find("UTF") != std::string::npos);
+      }
+
+      // Test another string with a decimal point.
+      local_dec_float_type d_other("123.789");
+      str = d_other.str();
+
+      BOOST_CHECK(str == "123.789");
    }
-   catch(const std::runtime_error&){}  // No en-US locale
+   catch(const std::runtime_error&) { }  // LCOV_EXCL_LINE No locale was found.
 
-   BOOST_CHECK(!str.empty());
-   BOOST_CHECK(str.find('.') != std::string::npos);
+   BOOST_CHECK(str == "123.789");
 
    return boost::report_errors();
 }
