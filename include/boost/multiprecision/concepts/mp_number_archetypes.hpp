40cf9e50603a27b073a3349bdefa7b838f550405
ckormanyos <ckormanyos@yahoo.com>
2026-04-07 23:01:55 +0200

    Relax constexpr-ness of a std-function

--- include/boost/multiprecision/concepts/mp_number_archetypes.hpp
+++ include/boost/multiprecision/concepts/mp_number_archetypes.hpp
@@ -212,7 +212,7 @@ inline int eval_fpclassify(const number_backend_float_architype& arg)
    return BOOST_MP_FPCLASSIFY(arg.m_value);
 }
 
-constexpr std::size_t hash_value(const number_backend_float_architype& v)
+inline BOOST_MP_CXX14_CONSTEXPR std::size_t hash_value(const number_backend_float_architype& v)
 {
    std::hash<long double> hasher;
    return hasher(v.m_value);
